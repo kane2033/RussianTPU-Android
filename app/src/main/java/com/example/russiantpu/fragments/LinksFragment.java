7c1ccbb8f1e54e7435bf07aa6a9cd881fc097e09
kane2033 <u-cent@yandex.ru>
2020-06-06 16:51:54 +0700

    Добавлен тестовый GET запрос с помощью библиотеки OkHttpClient, а также добавлена библиотека GSON для будущей десериализации в объекты.

--- app/src/main/java/com/example/russiantpu/fragments/LinksFragment.java
+++ app/src/main/java/com/example/russiantpu/fragments/LinksFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -16,14 +15,21 @@ import com.example.russiantpu.DataAdapter;
 import com.example.russiantpu.R;
 import com.example.russiantpu.utility.LinkItem;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 //фрагмент, отображающий список статей (новостей)
 public class LinksFragment extends Fragment {
 
-    private TextView textView;
-    private String message;
     private int selectedItemId;
 
     private List<LinkItem> items = new ArrayList<>();
@@ -52,14 +58,39 @@ public class LinksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         RelativeLayout layoutInflater = (RelativeLayout)inflater.inflate(R.layout.fragment_links, container, false);
-        //textView = layoutInflater.findViewById(R.id.nav_links_text);
-        //message = getArguments().getString("linksMessage", "null");
-        //textView.setText(message);
+        final TextView textView = layoutInflater.findViewById(R.id.nav_links_text);
+        //HTTP запрос (пока тестовый)
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://reqres.in/api/users/2")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(myResponse);
+                            //нужно десериализовать в объект с помощью gson.fromJson()
+                        }
+                    });
+                }
+            }
+        });
+        //вставка полученного результата из запроса
+
         selectedItemId = getArguments().getInt("id", 1);
-
         items = getItemsById(selectedItemId);
-
         RecyclerView recyclerView = layoutInflater.findViewById(R.id.list); //список
+
         //создаем адаптер
         DataAdapter adapter = new DataAdapter(this.getContext(), items);
         //устанавливаем для списка адаптер
